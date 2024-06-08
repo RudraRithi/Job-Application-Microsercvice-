e87d4ea2550170c1cf765a68a2d895e387027ae7
RudraRithi <rudrarithi07@gmail.com>
2024-06-08 17:05:18 +0530

    Add files via upload

---
+++ service-regi/src/test/java/service_regi/ServiceRegiApplicationTests.java
@@ -0,0 +1,13 @@
+package service_regi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
